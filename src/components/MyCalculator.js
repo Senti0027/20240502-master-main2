5dd779661888972252c10047ce6db11b7b32a1e0
Senti0027 <5b0g0027@stust.edu.tw>
2024-05-29 14:00:29 +0800

    fine

--- src/components/MyCalculator.js
+++ src/components/MyCalculator.js
@@ -1,56 +1,74 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./MyCalculator.css"
 import MyDisplay from "./MyDisplay";
 import MyButton from "./MyButton";
 
-function MyCalculator(){
-        const [result,setResult]=useState("0");
-        const handleClick=(value)=>{
-            switch (value) {
-                case "0":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case ".":
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                    if(result=="0"){
-                        setResult(result);
+function MyCalculator() {
+    const [result, setResult] = useState("0");
+    const handleClick = (value) => {
+        switch (value) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                if (result == "0") {
+                    setResult(value);
 
-                    }else{
-                        setResult(result+value);
-                    }
-                    break;
-                case "=":
+                }
+                else {
+                    setResult(result + value);
+                }
+                break;
+            case ".":
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if (result != "") {
+                    setResult(result + value);
+
+                }
+                break;
+            case "=":
+                if (result != "") {
+
                     setResult(eval(result).toString());
-                    break;
-                case "c":
+                }
+                break;
+            case "c":
+                if (result != "") {
+
                     setResult("");
-                    break;
-                case "+/-":
+                }
+                break;
+            case "+/-":
+                if (result != "") {
+
                     setResult(parseInt(result, 10) * -1);
-                    break;
-                case "%":
+                }
+                break;
+            case "%":
+                if (result != "") {
+
                     setResult(parseInt(result, 10) / 100);
-                    break;
-                    default:
-                    break;
-                    }
-        };
-    
-    return(
+                }
+                break;
+            default:
+                break;
+        }
+    };
+
+    return (
         <div className="calculator">
             <h1>計算結果</h1>
-            <MyDisplay result={result}/>
-            <MyButton buttonClicked={handleClick}/>
+            <MyDisplay result={result} />
+            <MyButton buttonClicked={handleClick} />
         </div>
     );
 }
