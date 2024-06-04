74ebe8a4bd78caa360913b1d68b6b21c9dcd3498
Senti0027 <5b0g0027@stust.edu.tw>
2024-06-04 23:11:48 +0800

    yee

--- src/components/Tic-Tac-Toe/Tic-Tac-Toe.js
+++ src/components/Tic-Tac-Toe/Tic-Tac-Toe.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 import './tictactoe.css';
 
-function TicTacToe(){
-    return(
-        <div className="game">
-            <Board />
+function TicTacToe() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+    const xIsNext = currentMove % 2 === 0;
 
+
+    const handlePlay = (nextSquares) => {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
         </div>
     );
 
