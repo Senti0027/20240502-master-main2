696706dff24c01b2ed2cb2980c08530492b5f001
Senti0027 <5b0g0027@stust.edu.tw>
2024-06-12 15:05:43 +0800

    damedane

--- src/components/Calculator/MyDisplay.js
+++ src/components/Calculator/MyDisplay.js
@@ -1,6 +1,6 @@
-import React from'react';
+import React from 'react';
 
-function MyDisplay(props){
-    return(<div className='display'>{props.result}</div>);//"result"是用於傳遞結果或資料的 props 名稱，給MyDisplay
+function MyDisplay(props) {
+    return (<div className='display'>{props.result}</div>);//"result"是用於傳遞結果或資料的 props 名稱，給MyDisplay
 }
 export default MyDisplay;
