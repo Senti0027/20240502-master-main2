f52667f3b7bf7fd1d8b1f4d7a57c3ea282aff077
Senti0027 <5b0g0027@stust.edu.tw>
2024-06-14 22:36:30 +0800

    6

--- src/components/Calculator/MyDisplay.js
+++ src/components/Calculator/MyDisplay.js
@@ -2,6 +2,6 @@ import React from 'react';
 //顯示計算器的當前結果
 
 function MyDisplay(props) {
-    return (<div className='display'>{props.result}</div>);// 使用 props.result 顯示結果
+    return (<div className='display'>{props.result}</div>);// 使用 props.result 顯示結果dammedane
 }
 export default MyDisplay;
